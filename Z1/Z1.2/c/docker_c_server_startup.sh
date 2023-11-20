56a03f08c98181318d5f279232e01c254ebf338a
Michał Sobiech (Laptop) <michal.sobiech15@gmail.com>
2023-11-20 16:30:43 +0100

    Docker C Server setup runs

--- Z1/Z1.2/c/docker_c_server_startup.sh
+++ Z1/Z1.2/c/docker_c_server_startup.sh
@@ -2,14 +2,15 @@ set -e
 
 NAME='z41_z_1_c_server'
 
+docker stop $NAME
 if [[ "$(docker ps -aqf name=$NAME)" ]]; then
     docker rm $NAME
 fi
 
 docker build --tag $NAME:v1 .
 
-docker create -it --name $NAME --network z41_network $NAME:v1
+docker create -it --name $NAME --network z41_network --cap-add NET_ADMIN $NAME:v1
 
 docker ps -a
 
-docker start -i $NAME
+docker start $NAME
--- Z1/Z1.2/c/server_container_config.sh
+++ Z1/Z1.2/c/server_container_config.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+
 tc qdisc add dev eth0 root netem delay 1000ms 500ms loss 50%
+./server 8888
