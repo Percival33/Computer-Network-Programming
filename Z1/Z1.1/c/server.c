5345649fd9338c2ec9ab85659ce7faf54394c3a6
Michał Sobiech (Laptop) <michal.sobiech15@gmail.com>
2023-11-24 22:35:13 +0100

    1.2 server.c copied to 1.1

--- Z1/Z1.1/c/server.c
+++ Z1/Z1.1/c/server.c
@@ -6,20 +6,84 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <math.h>
+#include "common.h"
 
-#define ERROR_INVALID_ARGC 1
-#define ERROR_FAILED_SOCKET_CREATION 2
-#define ERROR_FAILED_SOCKET_BIND 3
-#define ERROR_FAILED_DATA_RECEIVAL 4
-#define ERROR_FAILED_TO_SEND_RESPONSE 5
+bool text_is_only_zeroes(char *text, int text_length) {
+    for (int i = 0; i < text_length; i++) {
+        if (text[i] != '\0') {
+            return false;
+        }
+    }
+    return true;
+}
 
-#define PAIR_SIZE 4
-#define BUFFER_SIZE 1024
+bool datagram_is_valid(data_t *packet_data) {
+    // Datagram is valid, if:
+    // - After the declared pairs there are only zeroes,
+    // - The number of pairs does not exceed the max number of
+    // pairs that can fit in a packet
+    
+    // TODO parametrize
+
+    int count = packet_data->count;
+    // Size
+    if (count > MAX_PAIR_COUNT) {
+        printf(LOG_ERROR "Datagram is not valid. MAX_PAIR_COUNT(%d) exceeded got: %d\n", MAX_PAIR_COUNT, count);
+        return false;
+    }
+
+    // Check if the remaining are equal to 0.
+    // IMPORTANT - THERE MUST ONLY BE FULL PAIRS
+    char *key;
+    char *value;
+    for (int i = count; i < MAX_PAIR_COUNT; i++) {
+        key = packet_data->pairs[i].key;
+        if (!text_is_only_zeroes(key, KEY_SIZE)) {
+            printf(LOG_ERROR"Datagram is not valid. Error parsing key[%d]\n", i);
+            return false;
+        }
+        value = packet_data->pairs[i].value;
+        if (!text_is_only_zeroes(value, VALUE_SIZE)) {
+            printf(LOG_ERROR"Datagram is not valid. Error parsing value[%d]=(%s)\n", i, value);
+            return false;
+        }
+    }
+    return true;
+}
+
+bool response_is_valid(char buffer[], int expected_id) {
+    // TODO parametrize
+    int packet_id = ntohs(((uint16_t)buffer[0] << 8) + (uint16_t)buffer[1]);
+    if (packet_id != expected_id) {
+        printf(LOG_ERROR"parsing datagram id. Got (%d) expected(%d)\n", packet_id, expected_id);
+        return false;
+    }
+    int status_code = (int) buffer[2];
+    if (status_code != 0) {
+        printf(LOG_ERROR"status. Got (%d) expected(%d)\n", status_code, 0);
+        return false;
+    }
+    return true;
+}
+
+void ntoh_on_packet_data(data_t *packet_data) {
+    packet_data->count = ntohs(packet_data->count);
+    packet_data->id = ntohs(packet_data->id);
+}
+
+void ntoh_on_response(response_t *response) {
+    response->id = ntohs(response->id);
+    response->status = ntohs(response->status);
+}
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         printf("Invalid argument count\n");
-        exit(ERROR_INVALID_ARGC);
+        exit(ERROR_INVALID_ARG);
     }
 
     int port = atoi(argv[1]);
@@ -28,6 +92,13 @@ int main(int argc, char *argv[]) {
     if (sockfd  < 0) {
         perror("Failed to create a socket");
         exit(ERROR_FAILED_SOCKET_CREATION);
+    }
+    int recvBufferSize = 1024 * 1024 * 2; // example buffer size: 2 MB
+
+    // Set the option
+    if (setsockopt(sockfd, SOL_SOCKET, SO_RCVBUF, &recvBufferSize, sizeof(recvBufferSize)) < 0) {
+        perror("setsockopt SO_RCVBUF failed");
+        // Handle error
     }
 
     struct sockaddr_in server_address;
@@ -43,40 +114,69 @@ int main(int argc, char *argv[]) {
         exit(ERROR_FAILED_SOCKET_BIND);
     }
 
-    char buffer[BUFFER_SIZE];
+    data_t packet_data;
     char client_ip_str[INET_ADDRSTRLEN];
     while(true) {
-        int data_length = recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr*)&client_address, &(socklen_t){sizeof(client_address)});
-        if (data_length == -1) {
-            perror("Failed to receive data");
-            exit(ERROR_FAILED_DATA_RECEIVAL);
-        }
+        message_args_t receive_msg_from_clients_args = {
+            sockfd,
+            &packet_data,
+            sizeof(packet_data),
+            &client_address
+        };
+        receive_message(&receive_msg_from_clients_args);
+        ntoh_on_packet_data(&packet_data);
+
+        // Parse the client's address
         inet_ntop(AF_INET, &(client_address.sin_addr), client_ip_str, sizeof(client_ip_str));
-        printf("Data received from %s:%d\n", client_ip_str, ntohs(client_address.sin_port));
+        printf(LOG_INFO"Data received from %s:%d\n", client_ip_str, ntohs(client_address.sin_port));
 
-        // The first 2 bytes are the size
-        uint16_t pair_count = ntohs(((uint16_t)buffer[0] << 8) + (uint16_t)buffer[1]);
+        if (datagram_is_valid(&packet_data)) {
+            printf("Number of pairs: %d\n", packet_data.count);
+            printf("Packet id: %d\n", packet_data.id);
+            printf("Pairs: \n");
+            char printable_key[KEY_SIZE + 1];
+            char printable_value[VALUE_SIZE + 1]; 
+            printable_key[KEY_SIZE] = '\0';
+            printable_value[VALUE_SIZE] = '\0';
+            for (int i = 0; i < packet_data.count; i++) {
+                memcpy(printable_key, packet_data.pairs[i].key, KEY_SIZE);
+                memcpy(printable_value, packet_data.pairs[i].value, VALUE_SIZE);
 
-        printf("Number of pairs: %d\n", pair_count);
-
-        int current_byte_no = 2;
-        char key[PAIR_SIZE/2];
-        char value[PAIR_SIZE/2];
-        for (int i = 0; i < 1; i++) {
-            strncpy(key, &buffer[current_byte_no], sizeof(key));
-            strncpy(value, &buffer[current_byte_no + sizeof(key)], sizeof(key));
-            current_byte_no += sizeof(key) + sizeof(value);
-            printf("%s : %s\n", key, value);
+                printf("%s:%s\n", printable_key, printable_value);
+            }
+            printf("\n");
         }
-
-        // TODO check if datagram is valid
-
-        char response[] = "test";
-        if (sendto(sockfd, response, sizeof(response), 0, (struct sockaddr*) &client_address, sizeof(client_address)) == -1) {
-            perror("Failed to send a response");
-            exit(ERROR_FAILED_TO_SEND_RESPONSE);
-        }
+        
+        // Start a timer on one thread.
+        // It will periodically resend the message...
+        response_t response_to_client = {
+            (uint16_t) htons(packet_data.id),
+            (uint8_t) htons(0)
+        };
+        message_args_t send_res_to_client_args = {
+            sockfd,
+            &response_to_client,
+            sizeof(response_to_client),
+            &client_address
+        };
+        pthread_t resender_thread;
+        resender_args_t resender_args = {
+            &send_res_to_client_args,
+            false
+        };
+        pthread_create(&resender_thread, NULL, resender, (void *)&resender_args);
+        
+        // ... and start listening for a response from the main thread.
+        response_t response_from_client;
+        message_args_t receive_res_from_client_args = {
+            sockfd,
+            &response_from_client,
+            sizeof(response_from_client),
+            &client_address
+        };
+        receive_message(&receive_res_from_client_args);
+        resender_args.message_received = true;
+        printf("Handshake completed\n");
     }
-
     exit(0);
 }
