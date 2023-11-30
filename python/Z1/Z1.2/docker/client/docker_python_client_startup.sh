b17225c725443bb3e79d7e8ea06c05b994d91940
Kacper Murygin <kacper.mur123@gmail.com>
2023-11-30 17:29:54 +0100

    edited sh script of starting docker container for client in python

--- python/Z1/Z1.2/docker/client/docker_python_client_startup.sh
+++ python/Z1/Z1.2/docker/client/docker_python_client_startup.sh
@@ -2,18 +2,27 @@
 
 set -e
 
-CONTAINER_NAME='z41_z_1_python_client'
+CLIENT_CONTAINER_NAME='z41_z_1_python_client'
+SERVER_CONTAINER_NAME='z41_z_1_python_server'
 
-if [[ "$(docker ps | grep $CONTAINER_NAME)" ]]; then
-    docker stop $CONTAINER_NAME
+# Stop if running
+if [[ "$(docker ps | grep $CLIENT_CONTAINER_NAME)" ]]; then
+    docker stop $CLIENT_CONTAINER_NAME
 fi
 
-if [[ "$(docker ps -aqf name=$CONTAINER_NAME)" ]]; then
-    docker rm $CONTAINER_NAME
+# Remove the container
+if [[ "$(docker ps -aqf name=$CLIENT_CONTAINER_NAME)" ]]; then
+    docker rm $CLIENT_CONTAINER_NAME
 fi
 
-docker build --tag $CONTAINER_NAME:v1 .
+docker build --tag $CLIENT_CONTAINER_NAME:v1 .
 
-docker create -p 8888:8888/udp -p 8889:8889/udp -it --name $CONTAINER_NAME --network z41_network --cap-add NET_ADMIN $CONTAINER_NAME:v1
+SERVER_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $SERVER_CONTAINER_NAME)
+echo $SERVER_IP
 
-docker start -ai $CONTAINER_NAME
+docker run -d \
+    --name $CLIENT_CONTAINER_NAME \
+    --network z41_network \
+    --cap-add NET_ADMIN \
+    --env SERVER_IP="$SERVER_IP" \
+    $CLIENT_CONTAINER_NAME:v1
