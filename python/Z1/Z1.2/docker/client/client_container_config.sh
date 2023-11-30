ca19b647c8b4bd0a6ba2b1b8b167b6128274f44a
bjasinsk <01169292@pw.edu.pl>
2023-11-30 13:11:10 +0100

    change paths

--- python/Z1/Z1.2/docker/client/client_container_config.sh
+++ python/Z1/Z1.2/docker/client/client_container_config.sh
@@ -1,3 +1,4 @@
 # ./client z41_z_1_c_server 8888
 # python3 ./client.py
-echo "hello World"
+# echo "hello World"
+python3 ./client.py '127.0.0.1' 8888
--- python/Z1/Z1.2/docker/server/server_container_config.sh
+++ python/Z1/Z1.2/docker/server/server_container_config.sh
@@ -3,4 +3,5 @@
 # tc qdisc add dev eth0 root netem delay 1000ms 500ms loss 50%
 
 python ./server.py '127.0.0.1' 8888
+
 # echo "hello World"
