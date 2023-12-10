6f78cef520d41c1211eadd2a57fa238276e29e27
Kacper Murygin <kacper.mur123@gmail.com>
2023-12-10 08:07:06 +0100

    deleted redundant variables

--- python/Z1/Z1.1/client.py
+++ python/Z1/Z1.1/client.py
@@ -12,7 +12,6 @@ MAX_PAYLOAD_SIZE = 2
 key_value_pair_t = struct.Struct('!2s2s')
 message_t = struct.Struct('!HH' + f'{4 * MAX_PAYLOAD_SIZE}s')
 
-
 def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("ip", type=str)
@@ -27,16 +26,9 @@ def parse_arguments(parser):
 def main():
     try:
         sockfd = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-        size = 1
-        key = 'A'
-        value = '1'
-
-        id = 1
-        count = 1
-        pairs = [('ef', '12'), ('rt', '34')]
-
-        few_pairs_packed = []
+        idx = 1
+        count = 5
+        pairs = [('ef', '12'), ('ef', '34'), ('ef', '34'), ('ef', '34'), ('ef', '34')]
 
         for k, v in pairs:
             one_pair_packed = key_value_pair_t.pack(k.encode(), v.encode())
