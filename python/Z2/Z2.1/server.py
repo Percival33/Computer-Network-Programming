8b8a00a60b001a98c02c6b78042ed0de8d7068b2
bjasinsk <01169292@pw.edu.pl>
2023-12-12 20:07:35 +0100

    change format of data in 2.1

--- python/Z2/Z2.1/server.py
+++ python/Z2/Z2.1/server.py
@@ -1,73 +1,61 @@
+from dataclasses import dataclass
 import socket
 import struct
-from dataclasses import dataclass
 
 @dataclass
 class Node:
-    """
-    uint16: number of nodes
-    uint16: value a
-    uint32: value b
-    uint16: length of string
-    uint8: string characters
-    """
     a: int
     b: int
     len: int
     text: str
+    next: 'Node' = None
 
-
-def pack(nodes_list):
-    packed_data = struct.pack('!H', len(nodes_list))
-    for node in nodes_list:
-        text_len = len(node.text)
-        node_data = struct.pack(f'!HIH{text_len}s', node.a, node.b, text_len, bytes(node.text, 'ascii'))
-        print(node_data)
-        b''.join([packed_data, node_data])
-
-    return packed_data
-
-
-def unpack(data) -> list[Node]:
+def unpack_data(data):
     print('data:', data)
-    node_number, = struct.unpack('!H', data[:2])
+    offset = 0
+    node_count, = struct.unpack_from('!H', data, offset)
+    offset += 2
 
-    unpacked_data = []
-    buf_ptr = 2
+    head = None
+    current = None
 
-    for _ in range(node_number):
-        a, b, text_len = struct.unpack('!HIH', data[buf_ptr:buf_ptr + 8])
-        buf_ptr += 8
+    for _ in range(node_count):
+        a, b, text_len = struct.unpack_from('!HIH', data, offset)
+        offset += 8
+        text, = struct.unpack_from(f'!{text_len}s', data, offset)
+        offset += text_len
+        new_node = Node(a, b, text_len, text.decode('utf-8'))
 
-        text, = struct.unpack(f'!{text_len}s', data[buf_ptr:buf_ptr + text_len])
-        buf_ptr += text_len
+        if not head:
+            head = new_node
+            current = head
+        else:
+            current.next = new_node
+            current = new_node
 
-        new_node = Node(a, b, text_len, text.decode('ascii'))
-        print(new_node)
-        unpacked_data.append(new_node)
+    return head
 
-    return unpacked_data
+def print_linked_list(head):
+    current = head
+    while current:
+        print(f"Node(a={current.a}, b={current.b}, len={current.len}, text='{current.text}')")
+        current = current.next
 
-
-def start_server(host='127.0.0.1', port=65432):
+def start_server(host='0.0.0.0', port=8888):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((host, port))
         s.listen()
         print(f"Server started, listening on {host}:{port}")
 
-        while True:
-            conn, addr = s.accept()
-            with conn:
-                print(f"Connected by {addr}")
-                while True:
-                    data = conn.recv(1024)
-                    if not data:
-                        break
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            data = conn.recv(1024)
+            if not data:
+                return
 
-                    unpacked = unpack(data)
-                    print(f"Received data: {unpacked}")
-                    conn.sendall(pack(unpacked))  # Echo back the received data
-                    print('ok')
+            head_node = unpack_data(data)
+            print_linked_list(head_node)
 
 
 if __name__ == "__main__":
