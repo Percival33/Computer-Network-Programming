07fb3a9b8c566dfe26d1ba7fbc723ff577391b95
Michał Sobiech (Laptop) <michal.sobiech15@gmail.com>
2023-12-20 00:21:29 +0100

    tests

--- c/Z2/Z2.3/generate_logs.sh
+++ c/Z2/Z2.3/generate_logs.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
 SEND_BUFFER_SIZE=$1
+if [ -z "$SEND_BUFFER_SIZE" ]; then
+    echo "INVALID ARG COUNT"
+    exit 1
+fi
+
 CLIENT_PROGRAM='./build/output/client'
 SERVER_IP='127.0.0.1'
 SERVER_PORT=8888
 LOGS_DIR="./experiment_2_logs/sndbuf_${SEND_BUFFER_SIZE}kB.txt"
 
+# Clear the file
+cat /dev/null > $LOGS_DIR
+
+# Fill the file with new data
 $CLIENT_PROGRAM $SERVER_IP $SERVER_PORT $SEND_BUFFER_SIZE >> $LOGS_DIR
