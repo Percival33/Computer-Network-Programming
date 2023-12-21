aeb2397366395fedae3df525cd78c792bec41af3
Marcin Jarczewski <marcin.jarc@gmail.com>
2023-12-21 15:53:58 +0100

    fix: register boards

--- project/main.py
+++ project/main.py
@@ -10,5 +10,6 @@ app.include_router(history_router, prefix="/history", tags=['history'])
 app.include_router(board_router, prefix="/board", tags=['board'])
 app.include_router(send_router, prefix="/send", tags=['send'])
 
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
