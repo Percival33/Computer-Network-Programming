84aed7513e61f66a5466d362b26e6e36198c01a1
Marcin Jarczewski <marcin.jarc@gmail.com>
2023-12-23 11:26:02 +0100

    feat: add settings class

--- project/beans.py
+++ project/beans.py
@@ -1,5 +1,9 @@
 from managers.CategoryManager import CategoryManager
 from managers.ConnectionManager import ConnectionManager
+from config import settings
+
+# TODO: DB connector example using settings
+# specific_db_connector = SpecificConnector(settings.DB_URL, settings.DB_NAME)
 
 category_manager = CategoryManager()
 connection_manager = ConnectionManager()
