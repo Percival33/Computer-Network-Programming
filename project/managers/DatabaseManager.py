51bd527dc5b062474b1897be06d8d22836624379
Michał Sobiech (Laptop) <michal.sobiech15@gmail.com>
2023-12-30 14:01:11 +0100

    More DB methods

--- project/managers/DatabaseManager.py
+++ project/managers/DatabaseManager.py
@@ -1,26 +1,72 @@
 import sqlite3
+from datetime import datetime
 from domain.Ad import Ad
+from domain.Category import Category
 
 
 class DatabaseManager:
-    def __init__(self, db_file: str):
+    def __init__(self, db_file: str) -> None:
         self.db_file = db_file
         self.setup()
 
 
-    def setup(self):
-        # Create the ads table if it does not exist
+    def setup(self) -> None:
+        self.setup_categories_table()
+        self.setup_ads_table()
+
+
+    def setup_ads_table(self):
+        # Create the table if it does not exist
         command = '''
             CREATE TABLE IF NOT EXISTS ads (
                 id INTEGER PRIMARY KEY,
                 text TEXT,
                 creation_date DATE
+                category_id INTEGER
+                FOREIGN KEY(category_id) REFERENCES categories(id)
             )
         '''
         self.execute_command(command)
 
 
-    def get_ads(self):
+    def setup_categories_table(self):
+        # Create the table if it does not exist
+        command = '''
+            CREATE TABLE IF NOT EXISTS categories (
+                id INTEGER PRIMARY KEY,
+                name TEXT
+            )
+        '''
+        self.execute_command(command)
+
+
+    def get_ads_by_category(self, category: str) -> list[Ad]:
+        command = f'''
+            SELECT * FROM ads
+            JOIN categories ON ads.category_id = categories.id
+            WHERE categories.name = {category} 
+        '''
+        return self.execute_command(command)
+
+
+    def get_categories(self) -> list[Category]:
+        command = '''
+            SELECT * FROM categories
+        '''
+        rows = self.execute_command(command)
+
+        categories: list[Category] = []
+        for row in rows:
+            # Id, name
+            category = Category(
+                id=row[0],
+                name=row[1]
+            )
+            categories.append(category)
+        return categories
+
+
+    def get_ads(self) -> list[Ad]:
         command = '''
             SELECT * FROM ads
         '''
@@ -34,11 +80,19 @@ class DatabaseManager:
                 text=row[1]
             )
             ads.append(ad)
-
         return ads
 
 
-    def execute_command(self, command: str):
+    def add_ad(self, text: str, creation_date: datetime) -> None:
+        creation_date_str = creation_date.strftime('%Y.%m.%d %H:%M:%S')
+        command = f'''
+            INSERT INTO ads (text, creation_date)
+            VALUES ({text}, {creation_date_str})
+        '''
+        rows = self.execute_command(command)
+
+
+    def execute_command(self, command: str) -> list[tuple]:
         connection = sqlite3.connect(self.db_file)
         cursor = connection.cursor()
 
