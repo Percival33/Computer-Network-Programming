630d83c60ce500deb1d64ea018da6be3cd4d5608
Kacper Murygin <kacper.mur123@gmail.com>
2024-01-01 17:44:33 +0100

    created structure for future DatabaseManager unit tests

---
+++ project/managers/test/test_DatabaseManager.py
@@ -0,0 +1,46 @@
+def test_init():
+    pass
+
+
+def test_setup():
+    pass
+
+
+def test_setup_ads_table():
+    pass
+
+
+def test_setup_categories_table():
+    pass
+
+
+def test_get_ads_by_category():
+    pass
+
+
+def test_get_categories():
+    pass
+
+
+def test_get_ads():
+    pass
+
+
+def test_add_ad():
+    pass
+
+
+def test_get_category_id_by_name():
+    pass
+
+
+def test_add_category():
+    pass
+
+
+def test_get_last_insert_id():
+    pass
+
+
+def test_execute_command():
+    pass
